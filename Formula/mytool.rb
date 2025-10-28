2f67b1af4296ca931b137660639d75cbe1006bcb
debugee <519306795@qq.com>
2025-10-28 17:47:30 +0800

    asdf

--- Formula/mytool.rb
+++ Formula/mytool.rb
@@ -2,7 +2,7 @@ class Mytool < Formula
   desc "Simple formula library and app example"
   homepage "https://github.com/debugee/mytool"
   url "https://github.com/debugee/mytool/archive/refs/tags/3.0.tar.gz"
-  sha256 "2423bb4e7fa460c13adcfc8de4cf9244d8554b6ac070347ca7a5ccf8c2a22791"
+  sha256 "6a14ccc0cf692338438150821de3a6d7116a43abad0b27ec810c7cc527c1e959"
   license "MIT"
 
   depends_on "cmake" => :build
